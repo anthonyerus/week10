0f16c466d6066f8fe1eff68b082e5d7b7bc7d351
anthonyerus <85246479+anthonyerus@users.noreply.github.com>
2021-08-05 10:39:25 +0100

    Create week10Assessment.js

---
+++ week10Assessment.js
@@ -0,0 +1,6 @@
+function spinalCase(str1)
+ { return str1.replace(/^[\W_]+|[\W_]+$|([\W_]+)/g,
+ 	
+function ($0, $1) { return $1 ? "-" : ""; }).replace(/([a-z])(?=[A-Z])/g, '$1-').toLowerCase(); } console.log(spinalCase("This Is Spinal Tab"));
+
+
